c31ee7001cf9dc1552ab93517dc3fa4cf478943a
Akash Rajpurohit <akash009.dev@gmail.com>
2018-12-26 12:17:53 +0530

    Single stream of symbols

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-let symbol;
-let symbolSize = 40;
+let symbolSize = 30;
+let stream;
 
 // Invoked once
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight)
 	background(17, 17, 17)
 
- 	// Initialize Symbol
-	symbol = new Symbol(
-		width / 2,
-		0,
-		random(4, 10)
-	)
-
-	// Generate that random symbol
-	symbol.setToRandomSymbol()
 	// Set symbol size
 	textSize(symbolSize);
+
+	// Create stream
+	stream = new Stream()
+
+	// Generate the symbols
+	stream.generateSymbols()
 }
 
 // get called repeatedly at 60FPS
@@ -24,7 +21,7 @@ function draw() {
 	// Redraw the background color
 	background(17, 17, 17)
 	// Draw the random symbol
-	symbol.renderSymbol();
+	stream.render();
 }
 
 function Symbol(x, y, speed) {
@@ -44,17 +41,42 @@ function Symbol(x, y, speed) {
 		}
 	}
 
-	// Render the symbol on screen
-	this.renderSymbol = function() {
-		fill(0 ,255, 80)
-		text(this.value, this.x, this.y)
-		this.rain()
-		this.setToRandomSymbol()
-	}
-
 	// Raining symbol
 	this.rain = function() {
 		// Reset the y for continous rain
 		(this.y >= height) ? this.y = 0 : this.y += this.speed
+	}
+}
+
+function Stream() {
+	this.symbols = []
+	this.totalSymbols = round(random(5, 25))
+	this.speed = round(random(5, 15))
+
+	// Generate array of symbols which will be part of a stream
+	this.generateSymbols = function() {
+		let y = 0
+		let x = width / 2
+
+		for(let i = 0; i <= this.totalSymbols; i++) {
+			// create new symbol
+			symbol = new Symbol(x, y, this.speed)
+			// set to random symbol
+			symbol.setToRandomSymbol()
+			// push to streams array
+			this.symbols.push(symbol)
+			// decrement the y by symbol size to ensure next symbol is generated just above the previous one
+			y -= symbolSize
+		}
+	}
+
+	// Display the stream
+	this.render = function () {
+		this.symbols.forEach(function(symbol) {
+			fill(0 ,255, 80)
+			text(symbol.value, symbol.x, symbol.y)
+			symbol.rain()
+			symbol.setToRandomSymbol()
+		})
 	}
 }
